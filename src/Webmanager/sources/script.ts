39762354615714246b5e3e10614ea7648aa33d89
Ismael Souza Silva <ismael1361@gmail.com>
2025-01-10 06:06:56 -0300

    adiciona comentário sobre a documentação de instalação do ActiveTable no arquivo script.ts

--- src/Webmanager/sources/script.ts
+++ src/Webmanager/sources/script.ts
@@ -2,6 +2,8 @@
 /// <reference types="./types/react"/>
 /// <reference types="./types/react-dom"/>
 
+//https://activetable.io/docs/installation/
+
 const icons = {
 	"plus": "M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z",
 	"delete": "M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z",
