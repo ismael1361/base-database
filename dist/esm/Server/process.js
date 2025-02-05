80c12f596bbda513b979aa4455c1f3b751191741
Ismael Souza Silva <ismael1361@gmail.com>
2025-02-05 00:27:54 -0300

    refatora gerenciamento do daemon; adiciona suporte para exibir informações do daemon; ajusta manipulação de PID e caminhos; remove arquivo de PID obsoleto

--- dist/esm/Server/process.js
+++ dist/esm/Server/process.js
@@ -9,29 +9,60 @@ function startDaemon(options) {
         console.log("Daemon is already running.");
         return;
     }
-    options.path = fs.existsSync(path.resolve(options.path)) ? path.resolve(options.path) : path.resolve(process.cwd(), options.path);
+    options.path = path.resolve(process.cwd(), options.path);
     const daemon = spawn("node", [__filename, "start", "--daemon", "--port", options.port, "--host", options.host, "--path", options.path], {
         detached: true,
         stdio: "ignore",
         cwd: process.cwd(),
     });
     daemon.unref();
-    fs.writeFileSync(PID_FILE, daemon.pid?.toString() ?? "");
+    fs.writeFileSync(PID_FILE, JSON.stringify({
+        pid: daemon.pid?.toString() ?? "",
+        port: options.port,
+        host: options.host,
+        path: options.path,
+    }));
     console.log(`Daemon started with PID: ${daemon.pid}`);
     console.log(`Server running on http://${options.host}:${options.port}`);
-    console.log(`Path to settings files: ${options.path}`);
+    console.log(`Path to settings dir: ${options.path}`);
 }
 function stopDaemon() {
     if (fs.existsSync(PID_FILE)) {
-        const pid = fs.readFileSync(PID_FILE, "utf8");
+        const options = fs.readFileSync(PID_FILE, "utf8");
         try {
+            const { pid } = JSON.parse(options);
             process.kill(parseInt(pid), "SIGTERM"); // Encerra o processo
             console.log(`Daemon with PID ${pid} has been stopped.`);
         }
         catch (err) {
-            console.log(`Failed to stop daemon with PID ${pid}: ${err?.message ?? String(err)}`);
+            console.log(`Failed to stop daemon: ${err?.message ?? String(err)}`);
         }
         fs.unlinkSync(PID_FILE); // Remove o arquivo de PID
+    }
+    else {
+        console.log("No daemon is running.");
+    }
+}
+function workDaemon(options) {
+    console.log("Daemon is running in the background...");
+    // Código do daemon
+    const interval = setInterval(() => {
+        console.log("Daemon is still running...");
+    }, 5000);
+    // Lidar com o sinal de término (SIGTERM) para encerrar o daemon corretamente
+    process.on("SIGTERM", () => {
+        clearInterval(interval); // Parar o intervalo
+        console.log("Daemon is shutting down...");
+        process.exit(0); // Encerrar o processo
+    });
+}
+function infoDaemon() {
+    if (fs.existsSync(PID_FILE)) {
+        const options = fs.readFileSync(PID_FILE, "utf8");
+        const { pid, port, host, path } = JSON.parse(options);
+        console.log(`Daemon is running with PID: ${pid}`);
+        console.log(`Server running on http://${host}:${port}`);
+        console.log(`Path to settings dir: ${path}`);
     }
     else {
         console.log("No daemon is running.");
@@ -43,20 +74,10 @@ program
     .option("--daemon", "Run the process in the background")
     .option("--port <port>", "Port to run the server", "3000")
     .option("--host <host>", "Host to run the server", "localhost")
-    .option("--path <path>", "Path to setings files", "/")
+    .option("--path <path>", "Path to setings dir", "/server-db-settings")
     .action((options) => {
     if (options.daemon) {
-        console.log("Daemon is running in the background...");
-        // Código do daemon
-        const interval = setInterval(() => {
-            console.log("Daemon is still running...");
-        }, 5000);
-        // Lidar com o sinal de término (SIGTERM) para encerrar o daemon corretamente
-        process.on("SIGTERM", () => {
-            clearInterval(interval); // Parar o intervalo
-            console.log("Daemon is shutting down...");
-            process.exit(0); // Encerrar o processo
-        });
+        workDaemon(options);
     }
     else {
         startDaemon(options);
@@ -67,6 +88,12 @@ program
     .description("Stop the daemon")
     .action(() => {
     stopDaemon();
+});
+program
+    .command("show")
+    .description("Show information about the daemon")
+    .action(() => {
+    infoDaemon();
 });
 program.parse(process.argv);
 //# sourceMappingURL=process.js.map
--- src/Server/process.ts
+++ src/Server/process.ts
@@ -7,13 +7,23 @@ import { program } from "commander";
 
 const PID_FILE = path.join(__dirname, "meu-daemon.pid");
 
-function startDaemon(options: any) {
+interface StartDaemonOptions {
+	port: string;
+	host: string;
+	path: string;
+}
+
+interface PidInfo extends StartDaemonOptions {
+	pid: string;
+}
+
+function startDaemon(options: StartDaemonOptions) {
 	if (fs.existsSync(PID_FILE)) {
 		console.log("Daemon is already running.");
 		return;
 	}
 
-	options.path = fs.existsSync(path.resolve(options.path)) ? path.resolve(options.path) : path.resolve(process.cwd(), options.path);
+	options.path = path.resolve(process.cwd(), options.path);
 
 	const daemon = spawn("node", [__filename, "start", "--daemon", "--port", options.port, "--host", options.host, "--path", options.path], {
 		detached: true,
@@ -22,7 +32,15 @@ function startDaemon(options: any) {
 	});
 
 	daemon.unref();
-	fs.writeFileSync(PID_FILE, daemon.pid?.toString() ?? "");
+	fs.writeFileSync(
+		PID_FILE,
+		JSON.stringify({
+			pid: daemon.pid?.toString() ?? "",
+			port: options.port,
+			host: options.host,
+			path: options.path,
+		}),
+	);
 	console.log(`Daemon started with PID: ${daemon.pid}`);
 	console.log(`Server running on http://${options.host}:${options.port}`);
 	console.log(`Path to settings dir: ${options.path}`);
@@ -30,14 +48,43 @@ function startDaemon(options: any) {
 
 function stopDaemon() {
 	if (fs.existsSync(PID_FILE)) {
-		const pid = fs.readFileSync(PID_FILE, "utf8");
+		const options = fs.readFileSync(PID_FILE, "utf8");
 		try {
+			const { pid } = JSON.parse(options) as PidInfo;
 			process.kill(parseInt(pid), "SIGTERM"); // Encerra o processo
 			console.log(`Daemon with PID ${pid} has been stopped.`);
 		} catch (err) {
-			console.log(`Failed to stop daemon with PID ${pid}: ${(err as any)?.message ?? String(err)}`);
+			console.log(`Failed to stop daemon: ${(err as any)?.message ?? String(err)}`);
 		}
 		fs.unlinkSync(PID_FILE); // Remove o arquivo de PID
+	} else {
+		console.log("No daemon is running.");
+	}
+}
+
+function workDaemon(options: StartDaemonOptions) {
+	console.log("Daemon is running in the background...");
+
+	// Código do daemon
+	const interval = setInterval(() => {
+		console.log("Daemon is still running...");
+	}, 5000);
+
+	// Lidar com o sinal de término (SIGTERM) para encerrar o daemon corretamente
+	process.on("SIGTERM", () => {
+		clearInterval(interval); // Parar o intervalo
+		console.log("Daemon is shutting down...");
+		process.exit(0); // Encerrar o processo
+	});
+}
+
+function infoDaemon() {
+	if (fs.existsSync(PID_FILE)) {
+		const options = fs.readFileSync(PID_FILE, "utf8");
+		const { pid, port, host, path } = JSON.parse(options) as PidInfo;
+		console.log(`Daemon is running with PID: ${pid}`);
+		console.log(`Server running on http://${host}:${port}`);
+		console.log(`Path to settings dir: ${path}`);
 	} else {
 		console.log("No daemon is running.");
 	}
@@ -49,22 +96,10 @@ program
 	.option("--daemon", "Run the process in the background")
 	.option("--port <port>", "Port to run the server", "3000")
 	.option("--host <host>", "Host to run the server", "localhost")
-	.option("--path <path>", "Path to setings dir", "/")
+	.option("--path <path>", "Path to setings dir", "/server-db-settings")
 	.action((options) => {
 		if (options.daemon) {
-			console.log("Daemon is running in the background...");
-
-			// Código do daemon
-			const interval = setInterval(() => {
-				console.log("Daemon is still running...");
-			}, 5000);
-
-			// Lidar com o sinal de término (SIGTERM) para encerrar o daemon corretamente
-			process.on("SIGTERM", () => {
-				clearInterval(interval); // Parar o intervalo
-				console.log("Daemon is shutting down...");
-				process.exit(0); // Encerrar o processo
-			});
+			workDaemon(options);
 		} else {
 			startDaemon(options);
 		}
@@ -75,6 +110,13 @@ program
 	.description("Stop the daemon")
 	.action(() => {
 		stopDaemon();
+	});
+
+program
+	.command("show")
+	.description("Show information about the daemon")
+	.action(() => {
+		infoDaemon();
 	});
 
 program.parse(process.argv);
